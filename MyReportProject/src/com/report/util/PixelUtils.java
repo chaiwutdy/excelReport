fe8c3f822f5334bf018c9cdce03f86da6fc74d5c
tc02t076 <tc02t076@ATCIP02T076-1.ap.mds.honda.com>
2017-04-06 11:45:10 +0700

    update

--- MyReportProject/src/com/report/util/PixelUtils.java
+++ MyReportProject/src/com/report/util/PixelUtils.java
@@ -1,10 +1,9 @@
 package com.report.util;
 
 public class PixelUtils {
-	public static final short EXCEL_COLUMN_WIDTH_FACTOR = 256;
-  public static final short EXCEL_ROW_HEIGHT_FACTOR = 20;
-  public static final int UNIT_OFFSET_LENGTH = 7;
-  public static final int[] UNIT_OFFSET_MAP = new int[] { 0, 36, 73, 109, 146, 182, 219 };
+	 public static final short EXCEL_COLUMN_WIDTH_FACTOR = 256;
+	 public static final int UNIT_OFFSET_LENGTH = 7;
+	 public static final int[] UNIT_OFFSET_MAP = new int[] { 0, 36, 73, 109, 146, 182, 219 }; 
     /*public static final short EXCEL_COLUMN_WIDTH_FACTOR = 256;
     public static final short EXCEL_ROW_HEIGHT_FACTOR = 20;
     public static final int UNIT_OFFSET_LENGTH = 7;
@@ -29,15 +28,22 @@ public class PixelUtils {
         pixels += Math.floor((float) offsetWidthUnits / ((float) EXCEL_ROW_HEIGHT_FACTOR / UNIT_OFFSET_LENGTH));  
         return pixels;
     }*/
-	public static int widthUnits2Pixel(short widthUnits) {
-    int pixels = (widthUnits / EXCEL_COLUMN_WIDTH_FACTOR) * UNIT_OFFSET_LENGTH;
-    System.out.println("pixels:"+pixels);
+  /**
+   * excel width units(units of 1/256th of a character width) to pixel units
+   * @param widthUnits
+   * @return
+   */
+ /* public static int widthUnits2Pixel(short widthUnits) {
+    int pixels = (widthUnits * EXCEL_COLUMN_WIDTH_FACTOR);
+    System.out.println(pixels);
+  
     int offsetWidthUnits = widthUnits % EXCEL_COLUMN_WIDTH_FACTOR;
-    pixels += Math.floor((float) offsetWidthUnits / ((float) EXCEL_COLUMN_WIDTH_FACTOR / UNIT_OFFSET_LENGTH));  
+    pixels += Math.round((float) offsetWidthUnits / ((float) EXCEL_COLUMN_WIDTH_FACTOR / UNIT_OFFSET_LENGTH));
     return pixels;
-	}
+  } */
 	
-	public static int widthUnits2Pixel(int widthUnits) {
-		return (widthUnits * 256) + 200;
+	public static int widthUnits2Pixel(float widthUnits) {
+		//return (widthUnits * 256) + 150;//200;
+		return Math.round(widthUnits * 256) + 150;
 	}
 } 
